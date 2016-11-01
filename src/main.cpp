b88831f359966d048d1aa69ea78033ae7c87366d
Aidan O'Grady <aidan@aidanogrady.com>
2016-11-01 22:52:51 +0000

    Started rewrite in SFML, spent more time trying to figure out documentation layout. than writing working code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,149 +1,38 @@
-#include <SDL.h>
-#include <SDL_ttf.h>
-#include <iostream>
-#include <string>
+/******************************************************************************
+*
+ * @file    main.cpp
+ * @author  Aidan O'Grady
+ * @date    2016-11-01
+ * @version 0.1
+ *
+ * The controller of the system, creating the view and model of the application
+ * and handling interaction between them.
+ *
+ ******************************************************************************/
+#include <SFML/Window.hpp>
 #include "view.h"
-#include "keyboard.h"
-
-using namespace std;
-
-const int JOYSTICK_DEAD_ZONE = 16000;
-
-int x;
-int y;
-int size;
-
-/**
- * Refreshes the display, calling the view functions to render the keys, while
- * handling the logic of how the key is to be rendered.
- */
-void refresh()
-{
-    render_background();
-    for (int i = 0; i < ROWS; i++)
-    {
-        for (int j = 0; j < COLUMNS; j++)
-        {
-            string s(1, UPPER.keyboard[i][j]);
-
-            if (i >= y && i < y + size && j >= x && j < x + size)
-            {
-                render_key(i, j, s.c_str(), HOVER);
-            }
-            else {
-                render_key(i, j, s.c_str(), STANDARD);
-            }
-        }
-    }
-    update();
-}
-
-void joystick_event(SDL_Event* e)
-{
-    bool update = false;
-    switch (e->caxis.axis)
-    {
-        case SDL_CONTROLLER_AXIS_LEFTX:
-            if (e->caxis.value < -JOYSTICK_DEAD_ZONE)
-            {
-                x = (x - 1);
-                if (x < 0)
-                {
-                    x = COLUMNS - size;
-                }
-                update = true;
-            }
-            else if (e->caxis.value > JOYSTICK_DEAD_ZONE)
-            {
-                x = (x + 1) % (COLUMNS - size + 1);
-                update = true;
-            }
-            break;
-        case SDL_CONTROLLER_AXIS_LEFTY:
-            if (e->caxis.value < -JOYSTICK_DEAD_ZONE)
-            {
-                y = (y - 1);
-                if (y < 0)
-                {
-                    y = ROWS - size;
-                }
-                update = true;
-            }
-            else if (e->caxis.value > JOYSTICK_DEAD_ZONE)
-            {
-                y = (y + 1) % (ROWS - size + 1);
-                update = true;
-            }
-            break;
-    }
-
-    if (update) // Only want to redraw if necessary.
-    {
-        refresh();
-    }
-}
-
-void handle_event(SDL_Event* e)
-{
-    switch (e->type)
-    {
-        case SDL_CONTROLLERBUTTONUP:
-            cout << "Button up\n";
-            break;
-        case SDL_CONTROLLERBUTTONDOWN:
-            cout << "Button down\n";
-            break;
-        case SDL_CONTROLLERAXISMOTION:
-            joystick_event(e);
-            break;
-    }
-}
 
 /**
  * #include <SDL.h>
- * #include <iostream>
  *
  * The main control loop of the system, handling events etc.
  */
 int main(int argc, char* args[])
 {
-    x = 0;
-    y = 0;
-    size = 3;
-
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    View keyboard_view(1555, 525);
+     
+    while (keyboard_view.is_open())
     {
-        cout << "SDL_Error initialising: " << SDL_GetError() << "\n";
-        return -1;
-    }
-
-    if (!(view_init() && keyboard_init()))
-    {
-        cout << "Failed to initialize.\n";
-        return -1;
-    }
-
-    refresh();
-    
-    bool run = true;
-    SDL_Event e;
-    while (run)
-    {
-        while (SDL_PollEvent(&e) != 0)
+        sf::Event event = keyboard_view.get_event();
+        switch (event.type)
         {
-            if (e.type == SDL_QUIT)
-            {
-                run = false;
-            }
-            else
-            {
-                handle_event(&e);
-            }
+            case sf::Event::Closed:
+                keyboard_view.close();
+                break;
+            default:
+                break;
         }
-        SDL_Delay(67);
     }
 
-    keyboard_close();
-    view_close();
     return 0;
 }
--- src/view.h
+++ src/view.h
@@ -1,96 +1,51 @@
+/******************************************************************************
+*
+ * @file    view.cpp
+ * @author  Aidan O'Grady
+ * @date    2016-11-01
+ * @version 0.1
+ *
+ * Contains the components required for creating a window, and displaying
+ * information on the window.
+ *
+ ******************************************************************************/
 #ifndef VIEW_H
 #define VIEW_H
 
-/**
- * Structure for holding pixel dimensions of various on-screen objects. Each
- * size contains a width and height.
- */
-struct Size
+class View
 {
-    int w;
-    int h;
+private:
+    /**
+     * The SFML window that displays all information.
+     */
+    sf::Window window;
+
+public:
+    /**
+     * Constructs a new View.
+     *
+     * @param width the width of the View's window
+     * @param height the height of the View's window
+     */
+    View(int width, int height);
+
+    /**
+     * Waits for an Event to occur and returns.
+     *
+     * @return the first event from queue
+     */
+    sf::Event get_event();
+
+    /**
+     * Returns whether or not the view's window is open.
+     *
+     * @return whether or not the window is still open.
+     */
+    bool is_open();
+
+    /**
+     * Closes the view's window.
+     */
+    void close();
 };
-
-/**
- * The pixel gap between keys.
- */
-const int GAP = 5;
-
-/**
- * The gap required for the text box.
- */
-const int TEXT_BOX_HEIGHT = 100;
-
-/**
- * The size of the main window.
- */
-const struct Size WIN_SIZE = {1555, 525};
-
-/**
- * The responsive size of keys in pixels.
- */
-const struct Size KEY_SIZE = {150, 100};
-
-/**
- * The background colour of the keyboard.
- */
-const SDL_Color BKGRD = {21, 21, 21, SDL_ALPHA_OPAQUE};
-
-/**
- * The standard colour of keys.
- */
-const SDL_Color STANDARD = {117, 117, 117, SDL_ALPHA_OPAQUE};
-
-/**
- * The standard colour of keys.
- */
-const SDL_Color HOVER = {200, 230, 201, SDL_ALPHA_OPAQUE};
-
-/**
- * The colour of the characters being rendered.
- */
-const SDL_Color CHAR_COL = {255, 255, 255, SDL_ALPHA_OPAQUE};
-
-/**
- * The window of the keyboard.
- */
-extern SDL_Window* window;
-
-/**
- * The renderer of the keyboard, rendering the current layout of the keyboard.
- */
-extern SDL_Renderer* renderer;
-
-/**
- * The font used to render characters.
- */
-extern TTF_Font* font;
-
-/**
- * Initializes the system.
- *
- * Returns true if successful, otherwise false.
- */
-bool view_init();
-
-/**
- * Renders the given keyboard to the screen.
- */
-void render_background();
-
-/**
- * Renders the key at the given coordinates.
- */ 
-void render_key(int x, int y, const char* c, SDL_Color color);
-
-/**
- * Presents the current render to the window.
- */
-void update();
-
-/**
- * Performs a graceful shutdown of the system.
- */
-void view_close();
-
 #endif // VIEW_H
